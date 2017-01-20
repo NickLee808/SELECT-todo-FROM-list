7bfa5884b5be17cf0298d9ec6adb80e3f12d6829
Nick Lee <nicklee808@gmail.com>
2017-01-19 18:24:54 -1000

    5

--- todo_app.sql
+++ todo_app.sql
@@ -1,1 +1,9 @@
-DROP USER "michael";
+DROP USER IF EXISTS "michael";
+
+CREATE USER "michael" WITH ENCRYPTED PASSWORD 'stonebreaker';
+
+DROP DATABASE IF EXISTS "todo_app";
+
+CREATE DATABASE "todo_app";
+
+\c todo_app;
